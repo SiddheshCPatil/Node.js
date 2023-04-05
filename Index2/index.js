6a41d611d57e276af45972aa47754e52ef613d68
SiddheshCPatil <siddheshcp96896@gmail.com>
2023-04-05 08:47:20 +0530

    Index2

---
+++ Index2/index.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+
+// Making Folder
+// fs.mkdirSync("Index2")
+
+// Creating File in a folder and writing in it
+fs.writeFileSync("index2.txt", "Something Something happen ,you won't understand!");
+
+// Adding content to file
+fs.appendFileSync("index2.txt", " But who are you?");
+
+// Reading File
+data=fs.readFileSync("index2.txt","utf8");
+console.log(data);
+
+// Delete File
+fs.unlinkSync("index2.txt");
+
+// Delete Folder
+fs.rmdirSync("Index");
